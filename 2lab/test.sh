c9e9b5282f5c7b2eb71036e65c2000653ea9d288
Dmitry <mathmainfunction@gmail.com>
2024-05-26 21:51:22 +0300

    2 lab push

---
+++ 2lab/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Запустимо команду top у режимі потоків та збережемо результат у змінну
+top_output=$(top -H -b -n1)
+
+# Використаємо awk для обробки змінної
+echo "$top_output" | awk '
+BEGIN {
+    FS = " "
+    OFS = "\t"
+}
+NR > 7 {
+    if ($6 != "PR") {
+        prio = $6
+        virt_mem = $5
+        gsub(/K/, "", virt_mem)
+        virt_mem = virt_mem + 0
+
+        prio_virt_mem[prio] += virt_mem
+        total_virt_mem += virt_mem
+    }
+}
+END {
+    print "Priority\tVirtual Memory (KB)\tPercentage"
+    for (prio in prio_virt_mem) {
+        percentage = (prio_virt_mem[prio] / total_virt_mem) * 100
+        printf "%s\t\t%d\t\t%.2f%%\n", prio, prio_virt_mem[prio], percentage
+    }
+    printf "Total Virtual Memory: %d KB\n", total_virt_mem
+}
+'
